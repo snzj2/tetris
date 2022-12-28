13f8ef6cea2d0bfa624cdb242c9ab9ba377e388b
Ivanovichelovek <ivan.gerunov@mail.ru>
2022-12-29 00:37:59 +0500

    Изменил размеры поля (фигурки 35х35) Добавил load_level из урока добавил и немного изменил функцию start_screen доработаю её завтра

--- main.py
+++ main.py
@@ -1,5 +1,86 @@
+import os
+import sys
+
 import pygame
 from settings import *
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["Правила игры", "",
+                  "",
+                  "",
+                  ""]
+
+    settings_text = ["Кнопки в главном меню:", "",
+                     "/\ увеличить начальную скорость",
+                     "|| падения кубиков",
+                     "||",
+                     "",
+                     "",
+                     "|| уменьшить начальную скорость",
+                     "|| падения кубиков",
+                     "\/",
+                     "",
+                     f"скорость сейчас: {FPS}"]
+
+    controlers_text = ["Кнопки в игре:", "",
+                       "/\ Использовать бонус",
+                       "|| (Нарисован в верхнем левом углу",
+                       "||",
+                       "",
+                       "",
+                       "|| Увеличить скорость фигрурки до предела",
+                       "|| ",
+                       "\/",
+                       "",
+                       "-> подвинуть фигурку на 1 клуточку вправо",
+                       "",
+                       "<- подвинуть фигурку на 1 клуточку влево"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 if __name__ == '__main__':
@@ -8,6 +89,8 @@ if __name__ == '__main__':
     size = WIDTH, HEIGHT
     screen = pygame.display.set_mode(size)
     running = True
+    FPS = 5
+    clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
