5fa8e1b297c2d3dc61b3e6883f342b2eeae2ed0a
123 <baimuhametuv@gmail.com>
2022-12-29 23:56:23 +0500

    обновка картинок

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 WIDTH, HEIGHT = 550, 805
 FPS = 60
-figure = ["blue_g", "blue_square", "blue_stick", "blue_t", "blue_z",
-          "green_g", "green_square", "green_t", "green_z", "green_stick",
-          "red_g", "red_square", "red_tk", "red_z", "red_stick"]
+figure = ["blue_g", "blue_square", "blue_stick", "blue_t", "blue_z", "blue_grev", "blue_zrev",
+          "green_g", "green_square", "green_t", "green_z", "green_stick", "green_grev", "green_zrev",
+          "red_g", "red_square", "red_t", "red_z", "red_stick", "red_grev", "red_zrev",]
